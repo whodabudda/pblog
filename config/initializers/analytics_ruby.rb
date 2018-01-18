b0605569c9e577719c30360834436c36ce743c51
Paul Kneeland <whodabudda@gmail.com>
2018-01-18 10:12:23 -0800

    security change for analytics.

--- config/initializers/analytics_ruby.rb
+++ config/initializers/analytics_ruby.rb
@@ -2,4 +2,3 @@ Analytics = Segment::Analytics.new({
     write_key: 'NWrab8JF5GlNuxs67uXRpbE4OnY8qfXN',
     on_error: Proc.new { |status, msg| print msg }
 })
-# This is the prod write key:   yrSIgmV7XdrdA5Bbg0ZdPfEabfQOcqq4
