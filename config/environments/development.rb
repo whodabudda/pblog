7aea53c4f7dac0c96c15dfb0e30b2840286d666d
Paul Kneeland <whodabudda@gmail.com>
2018-12-06 10:59:17 -0600

    mysterious Devise::...force_ssl instructions

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  #in config/environments/production.rb
+config.to_prepare { Devise::SessionsController.force_ssl }
+config.to_prepare { Devise::RegistrationsController.force_ssl }
+config.to_prepare { Devise::PasswordsController.force_ssl }
+#
 end
