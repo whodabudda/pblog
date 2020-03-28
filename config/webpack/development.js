7a71bd4dec01f8f91d4b17742330b1e9eef7bc08
whodabudda <whodabudda@gmail.com>
2020-03-28 14:37:37 -0500

    Delete development.js

--- config/webpack/development.js
+++
@@ -1,8 +0,0 @@
-const environment = require('./environment')
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-
-process.env.VAPID_PUBLIC_DEV = 'BJHrwn78cnJSfvjoNeBdqAo9PLE21vaYMKaQh2NxQG9MmIx-E_CbSs89XYzZ8PEnbyqtQquC-yKQYqLm4RPHZX4='
-process.env.VAPID_PRIVATE_DEV = 'ra-oRqfwt1EqO-App7xgwL0UeAPbL0mZOAzp3lCxdxE='
-
-
-module.exports = environment.toWebpackConfig()
