ca4cc6e926de3ad2c5c4805bc5b4e1090ad9e893
Paul Kneeland <whodabudda@gmail.com>
2020-04-27 13:11:39 -0500

    add staging deploy

---
+++ config/webpack/staging.js
@@ -0,0 +1,10 @@
+const environment = require('./environment')
+process.env.NODE_ENV = process.env.NODE_ENV || 'staging'
+
+//may not need these keys here.  Can put them into an erb.js file if they are needed 
+//in a javascript. Otherwise, see if they can be confined to being used by just .rb files.
+//process.env.VAPID_PUBLIC = ''
+//process.env.VAPID_PRIVATE = ''
+
+
+module.exports = environment.toWebpackConfig()
