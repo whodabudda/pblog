142a0457610eb156814d5ef44566b99d8ebfcb6b
Paul Kneeland <whodabudda@gmail.com>
2020-04-27 13:06:50 -0500

    environment dependent config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,18 +1,15 @@
 #require 'sidekiq'
-#require 'sidekiq-status'
-
-#Sidekiq.configure_client do |config|
-#  config.client_middleware do |chain|
-#    chain.add Sidekiq::Status::ClientMiddleware
-#  end
-#  config.redis = { :namespace => 'pblog', :url => 'redis://127.0.0.1:6379/1' }
-#end
+#
+# ppk 04/16/2020
+# Sidekiq needs to know how to connect with redis.  Also, since the production or development system
+# may be running at the same time as stage, need to have a different DB number for redis.
+# The SIDEKIQ_REDIS_DB env variable is set in the 'local_env.yml' config file.
 
 Sidekiq.configure_server do |config|
-  config.redis = { :namespace => 'pblog', :url => 'redis://127.0.0.1:6379/0' }
+  config.redis = { :namespace => "pblog_#{Rails.env}", :url => "redis://127.0.0.1:6379/#{ENV["SIDEKIQ_REDIS_DB"]}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :namespace => 'pblog', :url => 'redis://127.0.0.1:6379/0' }
+  config.redis = { :namespace => "pblog_#{Rails.env}", :url => "redis://127.0.0.1:6379/#{ENV["SIDEKIQ_REDIS_DB"]}" }
 end
 
